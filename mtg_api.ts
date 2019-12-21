55cc4d502cd43eb3bdfde2cf96f2213a364adaa4
T0nixx <jiseok95@gmail.com>
2019-12-22 08:09:06 +0900

    Add: get_name_pairs

--- mtg_api.ts
+++ mtg_api.ts
@@ -8,6 +8,7 @@ import {
   MTG_API_Sets_Parameters,
   MTG_API_Sets_Response,
   MTG_API_Response,
+  Name_Pair,
 } from "./mtg_api_interfaces";
 
 const call_mtg_api = <T>({
@@ -42,40 +43,90 @@ const call_mtg_api = <T>({
   }[method](params);
 };
 
+const get_name_pair = (cards_with_korean: Name_Pair[], card: MTG_API_Card) => {
+  const ko_name = card.foreignNames.reduce<string>((str, foreign_name) => {
+    return foreign_name.language === "Korean" ? str + foreign_name.name : str;
+  }, "");
+  const result = { en_name: card.name, ko_name };
+
+  return ko_name !== "" ? [...cards_with_korean, result] : cards_with_korean;
+};
+
 (async () => {
-  // const arr = Array.from({ length: 15 }, (_, i) => i);
-  // console.log(arr);
-  const arr = [10, 3, 5];
-  return arr.map(i =>
-    call_mtg_api<MTG_API_Cards_Response>({
-      method: "cards",
-      params: { set: "ELD", page: i },
-    })
-      .then(res => {
-        // console.log(res.request);
-        const results = res.data.cards.reduce<
-          { name: string; ko_name: string }[]
-        >((cards_with_korean, card) => {
-          const ko_name = card.foreignNames.reduce<string>(
-            (str, foreign_name) => {
-              return foreign_name.language === "Korean"
-                ? str + foreign_name.name
-                : str;
-            },
-            "",
-          );
-          const result = { name: card.name, ko_name };
-          //console.log(result);
-          return ko_name !== ""
-            ? [...cards_with_korean, result]
-            : cards_with_korean;
-        }, []);
-        results.forEach(r => console.log(r));
-        return results;
-      })
-      .catch(e => e),
+  const first_repsonse = await call_mtg_api<MTG_API_Cards_Response>({
+    method: "cards",
+    params: { set: "ELD", page: 1 },
+  });
+
+  const total_count_for_this_set = first_repsonse.headers["total-count"];
+  console.log(total_count_for_this_set);
+  const pages_except_first = Array.from(
+    { length: total_count_for_this_set / 100 - 1 },
+    (_, i) => i + 2,
   );
-})();
+
+  const name_pair_matrix = await Promise.all(
+    pages_except_first.map(async i => {
+      // for throttling
+      await new Promise(resolve => setTimeout(resolve, 100));
+      const response = await call_mtg_api<MTG_API_Cards_Response>({
+        method: "cards",
+        params: { set: "ELD", page: i },
+      });
+
+      const en_ko_pairs = response.data.cards.reduce(get_name_pair, []);
+
+      return en_ko_pairs;
+    }),
+  );
+
+  const first_name_pairs = first_repsonse.data.cards.reduce(get_name_pair, []);
+  return Array.from(
+    new Set([
+      ...first_name_pairs,
+      ...name_pair_matrix.reduce(
+        (arr, name_pairs) => [...arr, ...name_pairs],
+        [],
+      ),
+    ]),
+  );
+})().then(r => console.log(r.length));
+
+// (async () => {
+//   const arr = Array.from({ length: 15 }, (_, i) => i);
+//   Promise.all(
+//     arr.map(i => {
+//       const timer = setTimeout(() => {}, i * 100);
+
+//       call_mtg_api<MTG_API_Cards_Response>({
+//         method: "cards",
+//         params: { set: "ELD", page: i },
+//       })
+//         .then(res => {
+//           const results = res.data.cards.reduce<
+//             { name: string; ko_name: string }[]
+//           >((cards_with_korean, card) => {
+//             const ko_name = card.foreignNames.reduce<string>(
+//               (str, foreign_name) => {
+//                 return foreign_name.language === "Korean"
+//                   ? str + foreign_name.name
+//                   : str;
+//               },
+//               "",
+//             );
+//             const result = { name: card.name, ko_name };
+
+//             return ko_name !== ""
+//               ? [...cards_with_korean, result]
+//               : cards_with_korean;
+//           }, []);
+//           console.log(i + " " + results);
+//           return results;
+//         })
+//         .catch(e => e.message);
+//     }),
+//   );
+// })();
 
 // (() => {
 //   call_mtg_api<MTG_API_Sets_Response>({
