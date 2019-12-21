c9a86ab2c64c91b5eb90566e50545ee002aca3b4
T0nixx <jiseok95@gmail.com>
2019-12-22 08:07:02 +0900

    Add: Scryfall interface

--- interfaces.ts
+++ interfaces.ts
@@ -22,3 +22,27 @@ export interface Card_Data {
   // scryfall uuid 한글을 뽑고싶은데
   id: string;
 }
+
+export interface Set_Data {
+  object: string;
+  id: string;
+  code: string;
+  name: string;
+  uri: string;
+  scryfall_uri: string;
+  search_uri: string;
+  released_at: string;
+  set_type: string;
+  card_count: number;
+  parent_set_code: string;
+  digital: boolean;
+  foil_only: boolean;
+  block_code: string;
+  block: string;
+  icon_svg_uri: string;
+}
+
+export interface Magic_Set {
+  name: string;
+  code: string;
+}
